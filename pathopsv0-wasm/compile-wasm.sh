6287e04203925666d34112b04eb4b4450ac30795
Erik Sombroek <eriksombroek@gmail.com>
2024-12-04 18:02:07 +0100

    Updated bindings

--- pathopsv0-wasm/compile-wasm.sh
+++ pathopsv0-wasm/compile-wasm.sh
@@ -32,7 +32,7 @@ fi
 echo "Building ES6"
 emcc $FLAGS -s EXPORT_ES6=1 -s NO_FILESYSTEM=1 -s ENVIRONMENT='web' -s MODULARIZE=1 -s WASM_BIGINT -s ALLOW_MEMORY_GROWTH=1 -s EXIT_RUNTIME=0 -O3 -s DISABLE_EXCEPTION_CATCHING=1 -s EXPORTED_RUNTIME_METHODS=addFunction,removeFunction -s EXPORTED_FUNCTIONS=_malloc,_free -s ALLOW_TABLE_GROWTH=1 \
     -I./pathopsv0/src -I. -I./pathopsv0/debug -DOP_TINY_TEST=1 -DNDEBUG \
-    ./pathopsv0/PathOps.cpp ./pathopsv0/src/*.cpp ./pathopsv0/debug/OpDebug.cpp ./pathopsv0-wasm/bindings.cpp \
+    ./pathopsv0/PathOps.cpp ./pathopsv0/src/*.cpp ./pathopsv0/debug/OpDebug.cpp ./pathopsv0/emscripten/Path2D.cpp ./pathopsv0-wasm/bindings.cpp \
     -o pathopsv0-wasm/dist/es/pathops.js \
     -s EXPORT_NAME="PathOps" \
     --bind
